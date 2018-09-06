04e03f8fcfa88e94f4a7ebbbf2143c842c49bc92
gezhan <gezhan@binghaodeMacBook-Pro.local>
2018-09-06 22:05:23 +0800

    端口更改

--- MRP/app/config/HttpRequest.js
+++ MRP/app/config/HttpRequest.js
@@ -11,9 +11,9 @@ import axios from 'axios';
 // '0' --- 测试服务器
 // '1' --- 过度服务器
 // '2' --- 正式服务器
-let isFormalServer = '2';
+let isFormalServer = '0';
 // 开发默认接口   正式服接口		http://d3e12907.ngrok.io / http://192.168.1.233:8588
-const BASEURL = isFormalServer === '2' ? 'http://192.168.1.82:9388/v1/' : 'http://192.168.1.82:9388/v1/';
+const BASEURL = isFormalServer === '2' ? 'http://192.168.1.82:9388/v1/' : 'http://47.99.73.98:8688/v1/';
 
 export default class HttpRequest extends Component {
 	static getBASEURL = () => {
