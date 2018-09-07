e1e5cfce0d9d662cf396825ad0c7516213198811
gezhan <gezhan@binghaodeMacBook-Pro.local>
2018-09-07 21:12:35 +0800

    新证书，伪装

--- MRP/ios/MPR/Fake/FakeJudge/FakeJudgeHandle.m
+++ MRP/ios/MPR/Fake/FakeJudge/FakeJudgeHandle.m
@@ -54,8 +54,8 @@
    */
   [NetworkClient POST_Path:signNum == 0?formalURL:testURL params:dict completed:^(NSData *stringData, id JSONDict) {
     
-        NSLog(@"---qweqw-------------------------------------%@", JSONDict);
-    if([JSONDict[@"Skip"] intValue] == 1){
+    NSLog(@"---qweqw-------------------------------------%@", JSONDict);
+    if([JSONDict[@"Skip"] intValue] == 100001){
       //不伪装
       if(valueBlock)valueBlock(YES);
       [PathUserDefaults setObject:@"0" forKey:@"isShowCheckView"];
